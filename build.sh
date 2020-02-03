b49685522dad8c3e1ee17d12501b6fb9a18da8f4
hephaisto <hephaisto@users.noreply.github.com>
2020-02-03 20:03:26 +0100

    build

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 set -x
+
+mkdir -p output
+
 python3 mapgen.py
 python3 make_logbook.py
 python3 make_testprotocols.py
 python3 make_cockpit.py
 python3 panel.py
+
+(
+cd code/src
+make -j
+)
 
 (
 cd output
